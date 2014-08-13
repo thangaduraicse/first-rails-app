ea53e834cc48e70e0570b6e2a6ef645c95cb162a
thangaduraicontractiq <thangadurai@contractiq.com>
2014-08-13 18:43:56 +0530

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+	before_create :create_remember_token
 	validates :name, presence: true, length:{ maximum: 50 }
 	VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true , format: { with: VALID_REGEX }, uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
-	#validates_confirmation_of :password, if: lambda { |m| m.password.present? }
+	def User.new_remember_token
+    	SecureRandom.urlsafe_base64
+  	end
+	def User.digest(token)
+	    Digest::SHA1.hexdigest(token.to_s)
+  	end
+
+  	private
+	def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@ describe "User" do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation)}
   it { should respond_to(:authenticate)}
-
+  it { should respond_to(:remember_token)}
 
   it { should be_valid }
 
@@ -95,4 +95,10 @@ describe "User" do
   before { @user.password = @user.password_confirmation = "a"*5}
   it { should_not be_valid}
  end
+
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+ end
+
 end
